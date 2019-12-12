99aeb647e4892f35ee9160905de537eaf937bc7d
abbhatia <abhishek.bhatia.123@gmail.com>
2019-12-12 19:49:51 +0000

    finish the problem

--- src/main/java/com/cgdecker/techlead_algopro/NonDuplicateNumber.java
+++ src/main/java/com/cgdecker/techlead_algopro/NonDuplicateNumber.java
@@ -1,8 +1,31 @@
 package com.cgdecker.techlead_algopro;
 
-public class NonDuplicateNumber { public static void main(String[] args) {
-    
-}
+import java.util.HashMap;
+import java.util.Map;
 
-private static 
+public class NonDuplicateNumber {
+    public static void main(String[] args) {
+            // System.out.println(singleNumber(new int[]{4, 3, 2, 4, 1, 3, 2}));
+            // System.out.println(singleNumber2(new int[]{4, 3, 2, 4, 1, 3, 2}));
+            System.out.println(singleNumber2(new int[]{1, 1, 2, 2, 3, 3,4, 5,4,5}));
+    }
+
+    private static int singleNumber(int[] nums) {
+        Map<Integer,Integer> map = new HashMap();
+        for(int i: nums){
+            map.put(i,map.getOrDefault(i, 0)+1);
+        }
+        return map.entrySet()
+        .stream()
+        .filter(e -> e.getValue() == 1).findFirst().map(e -> e.getKey()).get();
+
+    }
+    public static int singleNumber2(int[] nums) {
+        int result = 0;
+        for (int i : nums) {
+            result ^= i;
+            // System.out.println(result);
+        }
+        return result;
+    }
 }
