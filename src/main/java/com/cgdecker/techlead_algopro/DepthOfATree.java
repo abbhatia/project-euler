7a968cc91b5e9214026b53f0e12757bc0dfb2acf
abbhatia <abhishek.bhatia.123@gmail.com>
2019-12-12 23:40:53 +0000

    More changes

--- src/main/java/com/cgdecker/techlead_algopro/DepthOfATree.java
+++ src/main/java/com/cgdecker/techlead_algopro/DepthOfATree.java
@@ -2,12 +2,28 @@ package com.cgdecker.techlead_algopro;
 
 public class DepthOfATree{
     public static void main(String[] args) {
-        
+        Node root = new Node("a");
+
+        root.left = new Node("b");
+        root.right = new Node("c");
+        root.left.left = new Node("d");
+        root.left.left.right = new Node("e");
+
+        System.out.println(root);
+        System.out.println(getMaxDepth2(root,0));
+
+
     }
 
     public static int getMaxDepth(Node node) {
         if(node == null) return 0;
         else return 1 + Math.max(getMaxDepth(node.left), getMaxDepth(node.right));
+    }
+
+    public static int getMaxDepth2(Node node, int depth) {
+        if()
+        return Math.max(getMaxDepth2(node.left, depth + 1), getMaxDepth2(node.right, depth + 1));
+
     }
 
 }
@@ -18,5 +34,9 @@ class Node {
     public String value;
     public Node(String value) {
         this.value = value;
+    }
+    @Override
+    public String toString() {
+        return value;
     }
 }
